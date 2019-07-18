f4f8a3fc4d6a6400242bdf67370b7beb4f138867
netofalcao <apoioigneto@gmail.com>
2019-07-18 00:44:48 -0300

    venda funcionando, sem os valores do comprovante

--- main.c
+++ main.c
@@ -4,6 +4,7 @@
 #include <string.h>
 #include <math.h>
 #include <conio.h>
+#include <unistd.h>
 
 
 int main()
@@ -15,7 +16,18 @@ int main()
     int MM = 30;
     int val1;
     int val2;
-    int numCart;
+    int val3;
+    float valorProduto;
+    char numCart[14];
+    int length;
+
+
+    //PRODUTO1
+    char id[3] = "a";
+    int type = 1;
+    char label[30] = "CREDITO A VISTA";
+    float vMIn = 10.00;
+    float vMAX = 1000.00;
 
 
     printf("<%d> %d//%d %d:%d", terminalID, day, month, HH, MM);
@@ -27,12 +39,41 @@ int main()
 
         if (val1 == 1){
             //AREA DE VENDAS
-            printf("AREA DE VENDAS");
-            printf("ESCOLHA A VENDA \n 1- CREDITO A VISA \n 2-CREDITO PARCELADO \n 3 - DEBITO \n");
-            scanf("%d", val2);
+            printf("AREA DE VENDAS \n");
+            printf("ESCOLHA A VENDA \n 1- CREDITO A VISTA \n 2-CREDITO PARCELADO \n 3 - DEBITO \n");
+            scanf("%d", &val2);
             if (val2 == 1){
-                printf("\t <VENDA CREDITO A VISTA>");
-                int numCart;
+                printf("\t <VENDA CREDITO A VISTA> \n\n");
+                printf("Valor do produto\n");
+                scanf("%f", &valorProduto);
+                if (valorProduto < vMIn){
+                    printf("Valor Menor que o Minimo");
+                    break;
+                } else if(valorProduto > vMAX) {
+                    printf("Valor maior que o Maximo.");
+                    break;
+                } else {
+                    printf("Digite o numero do cartao\n");
+                    scanf("%s", numCart);
+                    length = strlen(numCart);
+                    if (length > 14 || length < 11){
+                        printf("Numero de cartao invalido\n");
+                        break;
+                    } else {
+                        printf("Deseja Efetuar a venda? (Y(1)/N(0) \n");
+                        scanf("%d", &val3);
+                         if(val3 == 0) {
+                            printf("Operação Cancelada \n");
+                            break;
+                        } else {
+                            printf("Venda Realizada com Sucesso \n\n\n");
+                            printf("\t <RAZAO SOCIAL> \n\t <ENDERECO> \n\t CNPJ: <CNPJ> \n DATA: DD/MM/AAAA \t TERMINAL: TTTTTT \n <ROTULO DO PRODUTO> \n <NUMERO DO CARTAO> \n");
+                            printf("VALOR APROVADO: <VALOR> R$<VALOR>");
+                            printf("\n\n <mensagem de rodape>");
+                        }
+                    }
+                }
+                //printf("VALOR (R$) \n %f", &valorProduto);
             }
 
             val1 = 0;
